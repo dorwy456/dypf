c9df7c3ef196af8441def453bfbcc4ed4b7aac6b
dorwy456 <dorwy456@gmail.com>
2020-03-09 18:02:39 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Root from "./App";
+import React from 'react';
+import ReactDOM from "react-dom"
+import Login from './components/page/Login/Login';
+import Dashboard from './components/organisms/dashboard/Dashboard';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    let isChange = false;
+    if(JSON.stringify(nextState) !== JSON.stringify(this.state)){
+      isChange = true;
+    }
+    return isChange
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      <Router>
+        <Switch>
+          <Route render={(props) => (
+            sessionStorage.getItem('isAuthenticated') === "true"
+            ? <Dashboard/>
+            : <Login/>
+          )}/>
+        </Switch>
+      </Router>
+      </React.Fragment>
+    );
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
